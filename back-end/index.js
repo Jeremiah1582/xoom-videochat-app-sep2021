4ef35723e8e9f0341d30d0aa024e1434271e73bf
jeremiah1582 <jeremiah.1582@googlemail.com>
2021-10-05 11:03:51 +0200

    removed the hard coded port in backend index.js -test

--- back-end/index.js
+++ back-end/index.js
@@ -56,7 +56,7 @@ const io = require("socket.io")(server, {
 
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
 	res.send('Running');
