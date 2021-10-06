6a9e5533d52f252f5a9a6f6c789f02340e155d9e
jeremiah1582 <jeremiah.1582@googlemail.com>
2021-10-06 11:40:25 +0200

    unnested Procfile from backend to root

--- back-end/index.js
+++ back-end/index.js
@@ -78,7 +78,7 @@ io.on("connection", (socket) => {
 	});
 });
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 
 
